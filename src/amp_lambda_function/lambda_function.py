bf2da05b401006671891a1fa26188b3b702fea91
abhishekn4 <abhishekn4@kpit.com>
2024-01-04 19:06:18 +0530

    resolving failures

--- src/amp_lambda_function/lambda_function.py
+++ src/amp_lambda_function/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from lamda ,deployed using Gitlab CI')
+        'body': json.dumps('Hello from lamda ,deployed using Github Actions')
     }
